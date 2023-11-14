426d37815f0db3f3c81269b075ade231991439ae
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2023-11-14 13:42:19 +0500

    init

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import Home from "./Home";
+
+const RootStack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="home" component={Home}/>
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
