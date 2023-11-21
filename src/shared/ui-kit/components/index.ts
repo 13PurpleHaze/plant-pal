74289e0b5994e63e297f8412df9df6083b5908a9
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2023-11-21 14:41:07 +0500

    feature/ui-kit config aliases for ui-kit

---
+++ src/shared/ui-kit/components/index.ts
@@ -0,0 +1,7 @@
+import Icon from './Icon';
+import Button from './Button';
+import CheckBox from './CheckBox';
+import TextField from './TextField';
+import CardReminder from './CardReminder';
+
+export { Icon, Button, CheckBox, TextField, CardReminder };
